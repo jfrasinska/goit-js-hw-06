1122ead3ab8ff148279c0ef2cb8194404299dee1
jfrasinska <j.frasinska@gmail.com>
2023-05-06 13:06:47 +0200

    adding eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const dataForm = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(dataForm);
+  event.currentTarget.reset();
+}
