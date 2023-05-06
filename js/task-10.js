da87a7d8106e03b02b66e1bdfb029863f507885b
jfrasinska <j.frasinska@gmail.com>
2023-05-06 14:36:22 +0200

    adding tenth task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,30 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const amountInput = document.querySelector("input");
+const amount = parseInt(amountInput.value);
+
+const boxesContainer = document.getElementById("boxes");
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = 30 + i * 10;
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.appendChild(box);
+  }
+}
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+
+createButton.addEventListener("click", () => {
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener("click", () => {
+  boxesContainer.innerHTML = "";
+});
